79a2e2edc9e94656e152a01e3ef588821341df89
도헌's <62533556+returnhakja@users.noreply.github.com>
2022-08-03 18:19:54 +0900

    feat : UI1

--- balloon/src/components/Dashboard.js
+++ balloon/src/components/Dashboard.js
@@ -1,73 +1,80 @@
 import { Card, CardContent, Container, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SideNavigation from './SideNavigation';
 
 const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography
-        sx={{ fontSize: 25 }}
-        color="#00AAFF"
-        gutterBottom
-        textAlign="center">
-        업무기안
-      </Typography>
-      <hr />
-      <br />
-      <Typography
-        sx={{ fontSize: 20 }}
-        variant="h5"
-        component="div"
-        textAlign="center">
-        업무기안입니다.
-      </Typography>
-    </CardContent>
-  </React.Fragment>
+  <Link to={'/dratf/br'}>
+    <React.Fragment>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 25 }}
+          color="#00AAFF"
+          gutterBottom
+          textAlign="center">
+          업무기안
+        </Typography>
+        <hr />
+        <br />
+        <Typography
+          sx={{ fontSize: 20 }}
+          variant="h5"
+          component="div"
+          textAlign="center">
+          업무기안입니다.
+        </Typography>
+      </CardContent>
+    </React.Fragment>
+  </Link>
 );
 const card1 = (
-  <React.Fragment>
-    <CardContent>
-      <Typography
-        sx={{ fontSize: 25 }}
-        color="#00AAFF"
-        gutterBottom
-        textAlign="center">
-        출장계획서
-      </Typography>
-      <hr />
-      <br />
-      <Typography
-        sx={{ fontSize: 20 }}
-        variant="h5"
-        component="div"
-        textAlign="center">
-        출장계획서입니다.
-      </Typography>
-    </CardContent>
-  </React.Fragment>
+  <Link to={'/dratf/bt'}>
+    <React.Fragment>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 25 }}
+          color="#00AAFF"
+          gutterBottom
+          textAlign="center">
+          출장계획서
+        </Typography>
+        <hr />
+        <br />
+        <Typography
+          sx={{ fontSize: 20 }}
+          variant="h5"
+          component="div"
+          textAlign="center">
+          출장계획서입니다.
+        </Typography>
+      </CardContent>
+    </React.Fragment>
+  </Link>
 );
 const card2 = (
-  <React.Fragment>
-    <CardContent>
-      <Typography
-        sx={{ fontSize: 25 }}
-        color="#00AAFF"
-        gutterBottom
-        textAlign="center">
-        인사명령
-      </Typography>
-      <hr />
-      <br />
-      <Typography
-        sx={{ fontSize: 20 }}
-        variant="h5"
-        component="div"
-        textAlign="center">
-        인사명령입니다.
-      </Typography>
-    </CardContent>
-  </React.Fragment>
+  <Link to={'/dratf/pa'}>
+    <React.Fragment>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 25 }}
+          color="#00AAFF"
+          gutterBottom
+          textAlign="center">
+          인사명령
+        </Typography>
+        <hr />
+        <br />
+        <Typography
+          sx={{ fontSize: 20 }}
+          variant="h5"
+          component="div"
+          textAlign="center">
+          인사명령입니다.
+        </Typography>
+      </CardContent>
+    </React.Fragment>
+  </Link>
 );
 
 function Dashboard() {
@@ -75,9 +82,11 @@ function Dashboard() {
     <div>
       <SideNavigation>
         <Container maxWidth="xl">
-          <p style={{ fontSize: '30px' }}>기본양식함</p>
+          <p style={{ fontSize: '2.1rem' }}>기본양식함</p>
+          <br />
           <hr />
-          <p style={{ fontSize: '25px' }}>기본양식 3</p>
+          <p style={{ fontSize: '1.5rem', marginTop: '2%' }}>기본양식 3</p>
+          <br />
           <Box
             sx={{
               display: 'flex',
@@ -88,24 +97,22 @@ function Dashboard() {
               p: 1,
               m: 1,
               bgcolor: 'background.paper',
-            }}
-            //   borderRadius: 1,
-          >
+            }}>
             <Card
               variant="outlined"
-              sx={{ minWidth: 400 }}
+              sx={{ minWidth: 300 }}
               style={{ backgroundColor: '#F1F9FF' }}>
               {card}
             </Card>
             <Card
               variant="outlined"
-              sx={{ minWidth: 400 }}
+              sx={{ minWidth: 300 }}
               style={{ backgroundColor: '#F1F9FF' }}>
               {card1}
             </Card>
             <Card
               variant="outlined"
-              sx={{ minWidth: 400 }}
+              sx={{ minWidth: 300 }}
               style={{ backgroundColor: '#F1F9FF' }}>
               {card2}
             </Card>
--- balloon/src/components/Login.js
+++ balloon/src/components/Login.js
@@ -22,60 +22,54 @@ const handleSubmit = (event) => {
 
 function Login() {
   return (
-    <div>
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 9,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            로그인
-          </Typography>
+    <Container component="main" maxWidth="xl" sx={{ height: '700px' }}>
+      <Box
+        sx={{
+          marginTop: 9,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          로그인
+        </Typography>
 
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              label="사원번호"
-              required
-              fullWidth
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              label="비밀번호"
-              type="password"
-              required
-              fullWidth
-              name="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="저장하기"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 4, mb: 2 }}>
-              Login{' '}
-            </Button>
-          </Box>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            label="사원번호"
+            required
+            fullWidth
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            label="비밀번호"
+            type="password"
+            required
+            fullWidth
+            name="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="저장하기"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 4, mb: 2 }}>
+            Login{' '}
+          </Button>
         </Box>
-      </Container>
-    </div>
+      </Box>
+    </Container>
   );
 }
 
