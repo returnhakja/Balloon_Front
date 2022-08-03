9bf2857036eacf53d10137f56aa3d6c1c0d66672
도헌's <62533556+returnhakja@users.noreply.github.com>
2022-08-03 18:24:02 +0900

    feat : fore

--- balloon/src/index.js
+++ balloon/src/index.js
@@ -9,20 +9,52 @@ import MainPage from './components/MainPage';
 
 import Boxs from './components/Boxs';
 import Dashboard from './components/Dashboard';
+import Declare from './components/Declare';
+import Complete from './components/Complete';
+import Save from './components/Save';
+import Refuese from './components/Refuese';
+import ApprovalBefore from './components/ApprovalBefore';
+import Calendar from './components/Calendar';
+import Businessreport from './components/Businessreport';
+import Businesstrip from './components/Businesstrip';
+import Persnelappointment from './components/Persnelappointment';
+import ApprovalOngoing from './components/ApprovalOngoing';
+import ApprovalComplete from './components/ApprovalComplete';
+import ApprovalRefuse from './components/ApprovalRefuse';
+import DocumentList from './components/DocumentList';
+import OrganizationChart from './components/OrganizationChart';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<MainPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/box" element={<Boxs />} />
-          <Route path="/dratf/form" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<MainPage />} />
+        {/* 로그인 */}
+        <Route path="/login" element={<Login />} />
+        {/* 캘린더 */}
+        <Route path="/calendar" element={<Calendar />} />
+        {/* 결재관리 */}
+        <Route path="/box" element={<Boxs />} />
+        <Route path="/box/dd" element={<Declare />} />
+        <Route path="/box/dc" element={<Complete />} />
+        <Route path="/box/ds" element={<Save />} />
+        <Route path="/box/dr" element={<Refuese />} />
+        <Route path="/box/ab" element={<ApprovalBefore />} />
+        <Route path="/box/ao" element={<ApprovalOngoing />} />
+        <Route path="/box/ac" element={<ApprovalComplete />} />
+        <Route path="/box/ar" element={<ApprovalRefuse />} />
+        <Route path="/box/dl" element={<DocumentList />} />
+        {/* 기안작성 */}
+        <Route path="/dratf/form" element={<Dashboard />} />
+        <Route path="/dratf/br" element={<Businessreport />} />
+        <Route path="/dratf/bt" element={<Businesstrip />} />
+        <Route path="/dratf/pa" element={<Persnelappointment />} />
+        {/* 조직도 */}
+        <Route path="/organization" element={<OrganizationChart />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
