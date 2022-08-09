40c4c8d9d936d12bd27b8b090505122029659226
UIJIN <62687865+kki45@users.noreply.github.com>
2022-08-09 20:38:31 +0900

    chatting mui update

--- balloon/src/pages/Chat/ChatRoom.js
+++ balloon/src/pages/Chat/ChatRoom.js
@@ -1,73 +1,85 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import './ChatRoom.css';
+import { useOutletContext } from 'react-router-dom';
+
+import Box from '@mui/material/Box';
+import ListItemButton from '@mui/material/ListItemButton';
+import Button from '@mui/material/Button';
+import ChatIcon from '@mui/icons-material/Chat';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Grid } from '@mui/material';
+import { Container } from '@mui/system';
 
 function ChatRoom() {
   const [chatroom, setChatroom] = useState([]);
-  const [chatList, setChatList] = useState([]);
+  const [empInfo, setEmpInfo, empId] = useOutletContext();
+  console.log(empId);
 
-  const onChatroom = (setChatroom) => {
-    axios.get('http://localhost:8080/allChat').then((response) => {
-      setChatroom(response.data);
-      console.log(response.data);
-    });
-  };
+  useEffect(() => {
+    const onChatroom = (setChatroom) => {
+      axios.get(`http://localhost:8080/allChat/${empId}`).then((response) => {
+        setChatroom(response.data);
+        console.log(response.data);
+      });
+    };
+    onChatroom(setChatroom);
+  }, []);
 
-  const onChatroomList = (setChatList) => {
-    axios.get('http://localhost:8080/allChatroom').then((response) => {
-      setChatList(response.data);
-    });
+  const onDeleteRoom = (chatroomId) => {
+    axios
+      .delete(`http://localhost:8080/deleteChatroom/${chatroomId}`)
+      .then((response) => console.log(response.data));
   };
 
   return (
-    <div>
-      <div className="header-title-container">
-        <h3>balloon의 채팅앱</h3>
-      </div>
+    <>
       <div>
-        <br />
-        <button
-          onClick={() => {
-            onChatroomList(setChatList);
-          }}>
-          채팅방이름만보기
-        </button>
-        <div>
-          <Link to={'/createroom'}>
-            <div>채팅방만들기</div>
-          </Link>
+        {/* 헤더 */}
+        <div className="header-title-container">
+          <h3>balloon의 채팅앱</h3>
+          <h3></h3>
         </div>
-        {chatList.map((list, index) => {
-          return <div key={index}>chatroomName : {list.chatroomName}</div>;
-        })}
+
+        {/* 채팅방 만들기 버튼 */}
         <div>
-          <hr />
-        </div>
-        <button
-          onClick={() => {
-            onChatroom(setChatroom);
-          }}>
-          모두보기
-        </button>
-        {chatroom.map((chat, index) => {
-          return (
-            <div key={index}>
-              chatroomName : {chat.chatroom.chatroomName}
-              <br />
-              headCount : {chat.chatroom.headCount}
-              <br />
-              chatContent : {chat.chatContent}
-              <br />
-              empId : {chat.employee.empId}
-              <Link to={`/chat?room=${chat.chatroom.chatroomId}`}>
-                <div>바로가기</div>
+          <Grid container justifyContent="flex-end">
+            <Button className="chatIcon">
+              <Link to={'/createroom'}>
+                <ChatIcon />
               </Link>
-            </div>
-          );
-        })}
+            </Button>
+          </Grid>
+        </div>
+
+        {/* 채팅방 목록보기 & 삭제하기 */}
+        <Container maxWidth="sm">
+          {chatroom.map((chat, index) => {
+            return (
+              <Box key={index} border="1px dashed blue">
+                <Link to={`/chat?room=${chat.chatroom.chatroomId}`}>
+                  <ListItemButton>
+                    {chat.chatroom.chatroomName}({chat.chatroom.headCount})
+                    <br />
+                    {chat.chatContent}
+                  </ListItemButton>
+                </Link>
+                {chat.employee.empId}
+
+                <Button
+                  variant="text"
+                  disableElevation
+                  onClick={() => onDeleteRoom(chat.chatroom.chatroomId)}>
+                  <DeleteIcon />
+                </Button>
+              </Box>
+            );
+          })}
+        </Container>
       </div>
-    </div>
+    </>
   );
 }
 export default ChatRoom;
