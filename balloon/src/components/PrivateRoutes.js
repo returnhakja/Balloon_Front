28f4b7e8d9f8851e2d6dbbda539e8259971415f3
Uno0306 <zion6878@naver.com>
2022-08-09 11:43:49 +0900

    f/l - Fix refresh error

--- balloon/src/components/PrivateRoutes.js
+++ balloon/src/components/PrivateRoutes.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { Link, useOutletContext } from 'react-router-dom';
 import { getCookie } from '../context/AuthFunc';
+import Cookies from 'universal-cookie';
 
 function PrivateRoutes({ empId, setEmpId, empInfo, setEmpInfo }) {
-  return !!empId ? (
+  const cookies = new Cookies();
+  const [cookie, setCookie] = useState({});
+  useEffect(() => {
+    cookies.get('accessToken');
+    setCookie(cookies.cookies);
+  }, []);
+
+  return !!cookie ? (
     <Outlet context={(setEmpId, empInfo, setEmpInfo)} />
   ) : (
     <Navigate to="/loginpage" />
