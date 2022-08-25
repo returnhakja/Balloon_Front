7264eba4e29023546feefdefb3be3a1a89b03674
UIJIN <62687865+kki45@users.noreply.github.com>
2022-08-25 15:01:50 +0900

    Refactor: "수정 채팅방 사원추가"
    채팅방에 없는 사원list를 불러와서 사원추가하는 기능

--- balloon/src/pages/chat/InviteEmp.js
+++ balloon/src/pages/chat/InviteEmp.js
@@ -6,6 +6,7 @@ import { Checkbox } from '@mui/material';
 import { useOutletContext } from 'react-router-dom';
 import {
   chatroomInfo,
+  empIdInfo,
   onHCInvite,
   onUserInvite,
 } from '../../context/ChatAxios';
@@ -15,10 +16,12 @@ import Stomp from 'stompjs';
 function InviteEmp({ style, modalOpen, setModalOpen }) {
   const [chatEmpList, setCEList] = useState([]);
   const [chatUnitList, setCUList] = useState([]);
-  const [invite, setInvite] = useState([]);
+  const [newInvite, setNewInvite] = useState([]);
+  const [existChatEmp, setECEList] = useState([]);
   //채팅방 정보 불러오기
   const [chatroomName, setChatroomName] = useState('');
   const [headCount, setHeadCount] = useState(0);
+  const [chatempinfo, setChatempinfo] = useState([]);
 
   const [disable, setDisable] = useState(false);
 
@@ -43,6 +46,12 @@ function InviteEmp({ style, modalOpen, setModalOpen }) {
   };
 
   ////////////////////////////////////////////////////////////
+  const existEmp = [];
+  chatempinfo.map((info) => {
+    existEmp.push(info.empId.empId);
+  });
+  console.log(existEmp);
+
   const returnArr = (list, setCUList) => {
     const arr = [];
     list.map((row) => {
@@ -55,41 +64,44 @@ function InviteEmp({ style, modalOpen, setModalOpen }) {
     return setCUList(array);
   };
 
+  //초대할 사원을 담아두는 메소드
+  const onInvite = (checked, data) => {
+    if (checked) {
+      setNewInvite([...newInvite, data]);
+    } else {
+      setNewInvite(newInvite.filter((button) => button !== data));
+    }
+  };
+
+  //사원정보가져오기
+  useEffect(() => {
+    chatroomInfo(chatroomId, setChatroomName, setHeadCount);
+    empIdInfo(chatroomId, setChatempinfo);
+  }, []);
+
+  //채팅방에 없는 사원list
+  const ChatEmpHandle = (chatEmpList, setECEList) => {
+    const arr = chatEmpList.filter((list) => !existEmp.includes(list.empId));
+    setECEList(arr);
+  };
+
   // 사원list 출력하기
   useEffect(() => {
     if (chatUnitList.length === 0) {
       if (chatEmpList.length === 0) {
         getEmpListInSameUnit(empId, setCEList);
-        setInvite([]);
+        setNewInvite([]);
       } else {
         setCUList(chatEmpList.unit);
         returnArr(chatEmpList, setCUList);
+
+        ChatEmpHandle(chatEmpList, setECEList);
       }
     }
-  }, [chatEmpList, chatUnitList]);
+  }, [chatEmpList, chatUnitList, existChatEmp]);
 
-  //초대할 사원을 담아두는 메소드
-  const onInvite = (checked, data) => {
-    if (checked) {
-      setInvite([...invite, data]);
-    } else {
-      setInvite(invite.filter((button) => button !== data));
-    }
-  };
-  console.log(invite);
-  console.log(chatroomId);
-  console.log(chatroomName);
-  console.log(headCount);
-
-  useEffect(() => {
-    chatroomInfo(chatroomId, setChatroomName, setHeadCount);
-  }, []);
-
-  console.log();
   const closemodal = () => {
-    // onUserInvite(chatroomId, invite, client);
     setModalOpen(false);
-    // onHCInvite(chatroomId, chatroomName, headCount, invite);
   };
 
   return (
@@ -97,36 +109,39 @@ function InviteEmp({ style, modalOpen, setModalOpen }) {
       <Box sx={style}>
         <h3>사원 초대하기</h3>
         <div className={styles.olList}>
-          {chatUnitList.map((cu, index) => {
-            return (
-              <div key={index} className={styles.cuCon}>
-                <p className={styles.cuName}>{cu}</p>
+          {chatUnitList.length !== 0 &&
+            chatUnitList.map((cu, index) => {
+              return (
+                <div key={index} className={styles.cuCon}>
+                  <p className={styles.cuName}>{cu}</p>
 
-                {chatEmpList.map((ce, index) => {
-                  if (ce.unit.unitName === cu) {
-                    return (
-                      <div key={index} className={styles.fontlist}>
-                        {ce.empName} {ce.position}
-                        <Checkbox
-                          type="checkbox"
-                          onChange={(e) => {
-                            onInvite(e.currentTarget.checked, ce);
-                          }}
-                          checked={invite.includes(ce) ? true : false}
-                        />
-                      </div>
-                    );
-                  }
-                })}
-              </div>
-            );
-          })}
+                  {existChatEmp.length !== 0 &&
+                    existChatEmp.map((ce, index) => {
+                      if (ce.unit.unitName === cu) {
+                        return (
+                          <div key={index} className={styles.fontlist}>
+                            {ce.empName} {ce.position}
+                            <Checkbox
+                              type="checkbox"
+                              onChange={(e) => {
+                                onInvite(e.currentTarget.checked, ce);
+                              }}
+                              checked={newInvite.includes(ce) ? true : false}
+                            />
+                          </div>
+                        );
+                      }
+                    })}
+                </div>
+              );
+            })}
         </div>
         <Button
           variant="contained"
           onClick={() => (
-            onUserInvite(chatroomId, invite, client),
-            onHCInvite(chatroomId, chatroomName, headCount, invite)
+            onUserInvite(chatroomId, newInvite, client),
+            onHCInvite(chatroomId, chatroomName, headCount, newInvite),
+            closemodal()
           )}>
           초대하기
         </Button>
