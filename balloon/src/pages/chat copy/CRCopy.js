0ba42e701317810f129356b55d88dc1dc274e00f
Uno0306 <zion6878@naver.com>
2022-08-29 16:46:32 +0900

    Feat: Create Messenger by modal

---
+++ balloon/src/pages/chat copy/CRCopy.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+import { onCreateChatroom2, onAllChatEmp } from '../../context/ChatAxios';
+import styles from '../../css/chat/Chat.module.css';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+import { Box, Modal } from '@mui/material';
+
+const styleBox = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 300,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  textAlign: 'center',
+  padding: 4,
+};
+
+function CRCopy({
+  invite,
+  openCreatChat,
+  setopenCreatChat,
+  empInfo,
+  setChatStatus,
+}) {
+  const inputRef = useRef();
+  // const [empInfo] = useOutletContext();
+  // socket
+  const sock = new SockJS('http://localhost:8080/chatstart');
+  const client = Stomp.over(sock);
+
+  client.connect({}, () => {
+    client.subscribe(`/topic/message`, (data) => {
+      disconnect();
+    });
+  });
+
+  const disconnect = () => {
+    client.disconnect();
+  };
+
+  const keyEnter = (e) => {
+    if (e.key === 'Enter') {
+      eventChatHandle();
+    }
+  };
+
+  //1:1채팅일 때 이미있는 채팅방 예외처리
+  const alreadyInvite = [];
+  invite.map((vite) => {
+    alreadyInvite.push(vite.empId);
+  });
+  console.log(alreadyInvite);
+
+  const [allChatEmp, setAllChatEmp] = useState([]);
+
+  useEffect(() => {
+    onAllChatEmp(setAllChatEmp);
+  }, []);
+
+  console.log(allChatEmp);
+  let allChatEmpId = [];
+  allChatEmpId = allChatEmp.filter((emp) => emp.empId.empId !== empInfo.empId);
+  console.log(allChatEmpId);
+
+  const checkChatEmp = (allChatEmpId, alreadyInvite) => {
+    let check = true;
+    allChatEmpId.map((id) => {
+      if (id.empId.empId == alreadyInvite) {
+        alert('이미 있는 채팅방입니다');
+        check = false;
+      }
+    });
+    return check;
+  };
+
+  const handleClose = () => setopenCreatChat(false);
+
+  const eventChatHandle = () => {
+    const input = document.getElementById('chatroomName');
+    console.log(input.value);
+    if (input.value.trim() !== '') {
+      console.log(input.value);
+      if (input.value.length === 0) {
+        inputRef.current.focus();
+        input.value = '';
+        alert('채팅방 이름을 입력해주세요!!');
+      } else {
+        if (alreadyInvite.length === 1) {
+          console.log('dddddddddddddddddddddddddd', alreadyInvite);
+          console.log(alreadyInvite.length);
+          const check = checkChatEmp(allChatEmpId, alreadyInvite[0]);
+          check &&
+            onCreateChatroom2(
+              empInfo,
+              invite,
+              document.getElementById('chatroomName'),
+              client,
+              setChatStatus
+            );
+        } else {
+          console.log('ssssssssssssssssssssssssss', alreadyInvite);
+          console.log(alreadyInvite.length);
+          onCreateChatroom2(
+            empInfo,
+            invite,
+            document.getElementById('chatroomName'),
+            client
+          );
+
+          handleClose();
+        }
+      }
+    } else {
+      inputRef.current.focus();
+      input.value = '';
+      alert('채팅방 이름을 입력해주세요!!');
+    }
+  };
+
+  return (
+    <Modal open={openCreatChat} onClose={handleClose}>
+      <Box sx={styleBox}>
+        <h3 className={styles.inBox}>채팅방 만들기</h3>
+        <div>
+          <Input
+            id="chatroomName"
+            className={styles.inBox}
+            ref={inputRef}
+            onKeyPress={keyEnter}
+            placeholder="채팅방 이름을 입력하세요"
+          />
+          <Button
+            variant="contained"
+            onClick={() => {
+              eventChatHandle();
+
+              // inputRef.current.value = '';
+            }}>
+            <div>채팅하기</div>
+          </Button>
+        </div>
+      </Box>
+    </Modal>
+  );
+}
+export default CRCopy;
