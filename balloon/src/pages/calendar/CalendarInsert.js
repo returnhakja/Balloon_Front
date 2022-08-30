5bab94be5b9e21ce74c4d6b740e59bf6280b19f5
UIJIN <62687865+kki45@users.noreply.github.com>
2022-08-30 13:49:47 +0900

    Refactor: "수정 코드리팩토링"

--- balloon/src/pages/calendar/CalendarInsert.js
+++ balloon/src/pages/calendar/CalendarInsert.js
@@ -1,17 +1,16 @@
 import { useEffect, useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
-import { insertSchedule, insertSchedulList } from '../../context/CalendarAxios';
-import styles from '../../css/Component.module.css';
-import { Box, Button, Modal, TextField, Typography } from '@mui/material';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+import { insertSchedulList } from '../../context/CalendarAxios';
 import {
   getEmpByEmpId,
   getEmpListInSameUnit,
-  setEmpInfoByEmpId,
 } from '../../context/EmployeeAxios';
+import { botChatroom } from '../../context/ChatAxios';
+import styles from '../../css/Component.module.css';
 import { BsCalendarWeek } from 'react-icons/bs';
+import { Box, Button, Modal, TextField, Typography } from '@mui/material';
 
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
 import axios from 'axios';
 
 function CalendarInsert({ style, openInsert, setOpenInsert, empInfo }) {
@@ -27,7 +26,6 @@ function CalendarInsert({ style, openInsert, setOpenInsert, empInfo }) {
 
   const handleClose = () => {
     setOpenInsert(false);
-    // window.location.href = '/calendar';
   };
 
   //사원추가 모달을 위한 open
@@ -41,10 +39,10 @@ function CalendarInsert({ style, openInsert, setOpenInsert, empInfo }) {
   };
 
   const handleempAddClose = () => {
-    if (inviteSchedule == 0) {
+    if (inviteSchedule.length === 0) {
       alert('사원을 추가 해 주세요');
     } else {
-      botChatroom();
+      botChatroom(inviteSchedule, setBotRoom);
       setOpen(false);
     }
   };
@@ -127,12 +125,6 @@ function CalendarInsert({ style, openInsert, setOpenInsert, empInfo }) {
 
   //이미 존재하는 사람들
   const [botRoom, setBotRoom] = useState([]);
-  const botChatroom = () => {
-    axios.post(`/cre/botchatroom`, inviteSchedule).then((response) => {
-      console.log(response.data);
-      setBotRoom(response.data);
-    });
-  };
 
   const botroomExist = [];
   const botroomId = [];
@@ -144,6 +136,7 @@ function CalendarInsert({ style, openInsert, setOpenInsert, empInfo }) {
   });
   console.log(botroomExist);
   console.log(botroomId);
+  console.log(inviteSchedule);
 
   //새로운 채팅방이 생성되어야할 사람들
   let invitepeople;
@@ -169,6 +162,7 @@ function CalendarInsert({ style, openInsert, setOpenInsert, empInfo }) {
 
   //chatroomEmployee T에 새로운 값넣고 채팅보내는 부분
   const onSchUserInvite = (add, invitepeople) => {
+    console.log(add);
     add.map((ad, index) => {
       axios
         .post(
