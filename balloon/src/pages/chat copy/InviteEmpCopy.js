6d21585ee84f3d833fc3ed0e7b51b2dc8250737e
Uno0306 <82020768+Uno0306@users.noreply.github.com>
2022-09-02 12:24:50 +0900

    Update InviteEmpCopy.js

--- balloon/src/pages/chat copy/InviteEmpCopy.js
+++ balloon/src/pages/chat copy/InviteEmpCopy.js
@@ -35,6 +35,7 @@ function InviteEmpCopy({
 
   // socket
   const sock = new SockJS('http://localhost:8080/chatstart');
+//   const sock = new SockJS('http://15.164.224.26:8080/chatstart');
   const client = Stomp.over(sock);
 
   client.connect({}, () => {
