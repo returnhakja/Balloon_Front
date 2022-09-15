35c838cdbd8abf3ba252cfa64fd9f2a202a8380e
도헌's <khs39200@gmail.com>
2022-09-15 17:27:25 +0900

    Feat:test

--- balloon/src/pages/chat/ChatStomp.js
+++ balloon/src/pages/chat/ChatStomp.js
@@ -2,7 +2,8 @@ import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
 function ChatStomp() {
-  const sock = new SockJS('http://localhost:8080/chatstart');
+  // const sock = new SockJS('http://localhost:8080/chatstart');
+  const sock = new SockJS('http://54.180.148.125:8080/chatstart');
   const client = Stomp.over(sock);
 
   client.connect({}, () => {
