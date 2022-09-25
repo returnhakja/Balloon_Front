a2b8f49c8da69ed93c04b3492798fae458ac0b33
SJHeon <sungjh20@gmail.com>
2022-09-25 19:15:07 +0900

    fix: Fix my code

--- balloon/src/pages/chat/ChatStomp.js
+++ balloon/src/pages/chat/ChatStomp.js
@@ -2,10 +2,10 @@ import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
 function ChatStomp() {
-//   const sock = new SockJS('http://localhost:8080/chatstart');
+  //   const sock = new SockJS('http://localhost:8080/chatstart');
   const sock = new SockJS('http://54.180.148.125:8080/chatstart');
   const client = Stomp.over(sock);
-  //   client.debug = null;
+  client.debug = null;
 
   return client;
 }
